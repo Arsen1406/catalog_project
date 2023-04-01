4051c9d7f8c9801dbcdc788d2c1e51484d64ba8c
Арсен Григорян <arsen8577711@yandex.ru>
2023-04-02 00:08:18 +0300

    Доделал тестовое задание

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_hio%le#cegb6&ig&*7)t$kgz7_vuk($0cxfi&lm5&(sy)l^=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
